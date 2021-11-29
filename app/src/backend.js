c3f19ac22ba12fb2cb84b398531dc3724a6c7318
Omar Faruque <ronymaha@gmail.com>
2021-11-29 23:21:12 +0600

    update

--- app/src/backend.js
+++ app/src/backend.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom"
 import style from './backend.scss'
 import FetchWP from './utils/fetchWP'
 import CSVReader from 'react-csv-reader'
+import CsvDownloader from 'react-csv-downloader'
+import ReactTooltip from "react-tooltip"
 
 
 import Csvloader from './utils/csvloader'
@@ -23,7 +25,115 @@ class App extends React.Component {
             csv_data: false, 
             type: 'single_choice_question', 
             assets_url: window.uiemlms_object.assets_url, 
-            upload_complete: false
+            upload_complete: false, 
+            csvArray: false, 
+            csv_columns: [
+                {
+                    id: 'user_id',
+                    displayName: __('User ID', 'user-import-export-mlms')
+                }, 
+                {
+                    id: 'user_name',
+                    displayName: __('Username', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'email',
+                    displayName: __('Email', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'role',
+                    displayName: __('Role', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'first_name',
+                    displayName: __('First Name', 'user-import-export-mlms'), 
+                },
+                {
+                    id: 'last_name',
+                    displayName: __('Last Name', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'degree',
+                    displayName: __('Degree', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'expertise',
+                    displayName: __('Expertise', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'submission_date',
+                    displayName: __('Submission Date', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'answer_date',
+                    displayName: __('Answer Date', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'message',
+                    displayName: __('Message', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'password',
+                    displayName: __('Password', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'send_reset_link',
+                    displayName: __('Send Password Reset Link', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'course_id',
+                    displayName: __('Course ID', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'date_of_enrollment',
+                    displayName: __('Date of Enrollment', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'course_progress',
+                    displayName: __('Course Progress', 'user-import-export-mlms'), 
+                },
+                {
+                    id: 'completed_lesson_id',
+                    displayName: __('Course Progress', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'reset_progress',
+                    displayName: __('Reset Progress', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'address',
+                    displayName: __('Address', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'organization',
+                    displayName: __('Organization', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'contact_number',
+                    displayName: __('Contact Number', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'order_by',
+                    displayName: __('Order By', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'order_no',
+                    displayName: __('Order No', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'order_key',
+                    displayName: __('Order Key', 'user-import-export-mlms'), 
+                },
+                {
+                    id: 'order_status',
+                    displayName: __('Order Status', 'user-import-export-mlms'), 
+                }, 
+                {
+                    id: 'order_date',
+                    displayName: __('Order Date', 'user-import-export-mlms'), 
+                }, 
+
+            ]
         }
 
         this.fetchWP = new FetchWP({
@@ -49,9 +159,28 @@ class App extends React.Component {
     }
 
 
-    componentDidMount() {}
+    componentDidMount() {
+        this.getConfig()
+    }
 
     componentWillUnmount() {}
+
+
+    getConfig(){
+        this.setState({
+            loader: true
+        });
+
+        this.fetchWP.get('userdata').then(json => {
+
+            this.setState({
+                loader: false, 
+                csvArray: json
+            });
+        }).catch(error => {
+            console.log('error is: ' , error);
+        })
+    }
 
     handleUpdate = () => {
         this.setState({
@@ -60,9 +189,6 @@ class App extends React.Component {
         const {csv_data} = this.state;
 
         this.fetchWP.post('save', {data: csv_data}).then(json => {
-
-
-            console.log('after success: ', json)
             this.setState({
                 loader: false, 
                 upload_complete: true
@@ -75,7 +201,7 @@ class App extends React.Component {
 
 
     render() {
-        const {config, upload_complete} = this.state;
+        const {csvArray, upload_complete} = this.state;
         return (
             <div className={style.bgWhite}>
                 {this.state.loader ? <Csvloader /> : null}
@@ -98,10 +224,33 @@ class App extends React.Component {
 
                 </div>
 
+
+                {/* Middle Part */}
+                <div>
+                        <div className={style.right_inner}>
+                            <label>{__('Export User Data as CSV:','user-import-export-mlms')}</label>
+                            
+                            <CsvDownloader 
+                                filename="user-import-export-mlms" 
+                                datas={csvArray} 
+                                // separator=";"
+                                wrapColumnChar=""
+                                columns={this.state.csv_columns}
+                                >
+                                <button className={style.export} data-tip data-for="exporttoCSVTip" onClick={ (e)=>{e.preventDefault()} }><span className={style.svgIcon + " dashicons dashicons-database-export"}></span>
+                                {__('Export to CSV', 'user-import-export-mlms')}
+                                </button>
+                                <ReactTooltip id="exporttoCSVTip" place="top" effect="solid">
+                                            {__('Table row export to CSV.', 'user-import-export-mlms')}
+                                </ReactTooltip>
+                            </CsvDownloader>
+                        </div>
+                </div>
+
                 {/* Right Part */}
                 <div>
                         <div className={style.right_inner}>
-                            <label>{__('Download CSV File:','user-import-export-mlms')}</label>
+                            <label>{__('Download CSV Demo File:','user-import-export-mlms')}</label>
                             <a href={this.state.assets_url + '/csv/msstudy-import-export-template.csv'} download >{__('Download CSV Sample', 'user-import-export-mlms')}</a>
                         </div>
                 </div>
--- includes/class-uiemlms-api.php
+++ includes/class-uiemlms-api.php
@@ -84,81 +84,189 @@ class UIEMLMS_Api
     public function testF(){
        
         
-        // $usermeta = get_user_meta( 3, 'become_instructor', true );
-        
-        // $usermeta = get_user_meta( 3, 'submission_date', true );
-        // echo 'submission date: ' . $usermeta . '<br/>';
+      
+        $users = get_users(  );
 
-        // echo 'display date: ' . date('M d, Y - h:i', $usermeta) . '<br/>';
-        // $usermeta = get_user_meta( 3 );
+        $users = array_map(function($v){            
+            $instructor_data = get_user_meta( $v->ID, 'become_instructor', true );
+            $submission_date = get_user_meta( $v->ID, 'submission_date', true ) ? date('m/d/Y', get_user_meta( $v->ID, 'submission_date', true )) : '';
 
-        // // $usermeta = get_user_meta( 3, 'submission_history', true);
+            $submission_history = get_user_meta( $v->ID, 'submission_history', true );
+            $answer_date = $submission_history && isset($submission_history['answer_date']) ? date('m/d/Y', $submission_history['answer_date']) : '';
 
-        // $postmea = get_post_meta( 2280 );
+            $userCourse = stm_lms_get_user_courses($v->ID, '', '', array('course_id', 'start_time', 'progress_percent'));
+            $cArray = wp_list_pluck($userCourse, 'course_id');
+            $sTime  = wp_list_pluck($userCourse, 'start_time');
+            $progress = wp_list_pluck( $userCourse, 'progress_percent' );
 
-        // $courses = STM_LMS_Curriculum::get_items_by_item(2286, array('course_id'));
+            foreach($sTime as $k => $singleTime) $sTime[$k] = date('m/d/Y', $singleTime);
+            foreach($progress as $s => $singleProgress){
+                if($singleProgress == 100)
+                    $progress[$s] = __('Completed', 'user-import-export-mlms');
+            }
 
-        // if(!empty($courses)){
-        //     $courses = wp_list_pluck($courses, 'course_id');
-        // }
+            $lessonsArray = array();
+            foreach($cArray as $sc){
+               $lessons = stm_lms_get_user_lessons($sc, array('lesson_id')); 
+               if($lessons){
+                   $lessons = wp_list_pluck( $lessons, 0);
+                   $lessonsArray = array_merge($lessons, $lessonsArray);
+               }
+            }
 
-        
-        // /*Get Courses where item included in curriculum*/
-        // $slesson = 2281;
-        // $user_id = 2;
-        // $enroll_courses = STM_LMS_Curriculum::get_items_by_item($slesson, array('course_id'));
+            $sTime = implode('-', $sTime);
+            
+            $cArray = implode('-', $cArray);
 
-        // if(!empty($enroll_courses)){
-        //     $enroll_courses = wp_list_pluck($enroll_courses, 'course_id');
-        //     foreach($enroll_courses as $sCourseid){
-        //         $user_course = STM_LMS_Helpers::simplify_db_array(stm_lms_get_user_course($user_id, $sCourseid, array(), $enterprise = ''));
-        //         // $user_course = array_map(function($v){
-        //         //     return $v['user_id'];
-        //         // }, $user_course);
-        //         echo 'user course <br/><pre>';
-        //         print_r($user_course);
-        //         echo '</pre>';
-        //     }
+            $progress = implode('-', $progress);
+
+            $lessonsArray = implode(',', $lessonsArray);
 
             
-        // }
+            $address = get_user_meta( $v->ID, 'billing_address_1', true ) ? get_user_meta( $v->ID, 'billing_address_1', true ) : get_user_meta( $v->ID, 'shipping_address_1', true );
+            
+            $company = get_user_meta( $v->ID, 'billing_company', true ) ? get_user_meta( $v->ID, 'billing_company', true ) : get_user_meta( $v->ID, 'shipping_company', true );
+
+            //Order
+            $order = STM_LMS_Order::_user_orders($v->ID);
+            $order_ids = $order ? wp_list_pluck($order['posts'], 'id') : array();
+            $order_keys = $order ? wp_list_pluck($order['posts'], 'order_key') : array();
+            $order_dates = $order ? wp_list_pluck($order['posts'], 'date') : array();
+            $order_status = $order ? wp_list_pluck($order['posts'], 'status') : array();
+
+            foreach($order_dates as $sd => $singD) $order_dates[$sd] = date('m/d/Y', $singD);
+
+            return array(
+                'ID' => $v->ID, 
+                'user_name' => $v->data->user_login, 
+                'email' => $v->data->user_email, 
+                'role' => $v->roles[0], 
+                'first_name' => get_user_meta( $v->ID, 'first_name', true ),
+                'last_name' => get_user_meta( $v->ID, 'last_name', true ), 
+                'degree' => $instructor_data ? $instructor_data['degree'] : '', 
+                'expertise' => $instructor_data ? $instructor_data['expertize'] : '',
+                'submission_date' => $submission_date, 
+                'answer_date' => $answer_date, 
+                'message' => isset($submission_history['message']) ? $submission_history['message'] : '', 
+                'password' => '', 
+                'send_reset_link' => '',
+                'course_id' => $cArray, 
+                'date_of_enrollment' => $sTime, 
+                'course_progress' => $progress, 
+                'completed_lesson_id' => $lessonsArray, 
+                'reset_progress' => '', 
+                'address' => $address, 
+                'organization' => $company,
+                'contact_number' => get_user_meta( $v->ID, 'billing_phone', true ), 
+                'order_by' => '', 
+                'order_no' => implode('-', $order_ids), 
+                'order_key' => implode('-', $order_keys), 
+                'order_status' => implode('-', $order_status), 
+                'order_date' => implode('-', $order_dates)
+            );
+        }, $users);
+
 
        
         
-        // $typeof = 'Completed';
-        
-
-        
-
-        // retrieve_password($getUser->data->user_login);
-        
-        $user_id = 3;
-        $cart_items = stm_lms_get_cart_items($user_id, apply_filters('stm_lms_cart_items_fields', array('item_id', 'price')));
-        echo 'cart items <br/><pre>';
-        print_r($cart_items);
-        echo '</pre>';
-
-        $price = get_post_meta( 2336 );
         echo 'metas <br/><pre>';
-        print_r($price);
+        // print_r($users);
+        print_r($users);
         echo '</pre>';
 
-        echo 'payment method code is: ' . get_option( 'pyment_m' ) . '<br/>';
-
-        // $cart_items = '';
-
-        // $invoice = STM_LMS_Order::create_order([
-        //     "user_id" => $user_id,
-        //     "cart_items" => $cart_items,
-        //     "payment_code" => $payment_code,
-        //     "_order_total" => $cart_total['total'],
-        //     "_order_currency" => $symbol
-        // ], true);
         
+       
+     
+       
         
     }
 
 
+
+    public function uiemlms_get_user_data(){
+        $users = get_users(  );
+
+        $users = array_map(function($v){            
+            $instructor_data = get_user_meta( $v->ID, 'become_instructor', true );
+            $submission_date = get_user_meta( $v->ID, 'submission_date', true ) ? date('m/d/Y', get_user_meta( $v->ID, 'submission_date', true )) : '';
+
+            $submission_history = get_user_meta( $v->ID, 'submission_history', true );
+            $answer_date = $submission_history && isset($submission_history['answer_date']) ? date('m/d/Y', $submission_history['answer_date']) : '';
+
+            $userCourse = stm_lms_get_user_courses($v->ID, '', '', array('course_id', 'start_time', 'progress_percent'));
+            $cArray = wp_list_pluck($userCourse, 'course_id');
+            $sTime  = wp_list_pluck($userCourse, 'start_time');
+            $progress = wp_list_pluck( $userCourse, 'progress_percent' );
+
+            foreach($sTime as $k => $singleTime) $sTime[$k] = date('m/d/Y', $singleTime);
+            foreach($progress as $s => $singleProgress){
+                if($singleProgress == 100)
+                    $progress[$s] = __('Completed', 'user-import-export-mlms');
+            }
+
+            $lessonsArray = array();
+            foreach($cArray as $sc){
+               $lessons = stm_lms_get_user_lessons($sc, array('lesson_id')); 
+               if($lessons){
+                   $lessons = wp_list_pluck( $lessons, 0);
+                   $lessonsArray = array_merge($lessons, $lessonsArray);
+               }
+            }
+
+            $sTime = implode('-', $sTime);
+            
+            $cArray = implode('-', $cArray);
+
+            $progress = implode('-', $progress);
+
+            $lessonsArray = implode(',', $lessonsArray);
+
+            
+            $address = get_user_meta( $v->ID, 'billing_address_1', true ) ? get_user_meta( $v->ID, 'billing_address_1', true ) : get_user_meta( $v->ID, 'shipping_address_1', true );
+            
+            $company = get_user_meta( $v->ID, 'billing_company', true ) ? get_user_meta( $v->ID, 'billing_company', true ) : get_user_meta( $v->ID, 'shipping_company', true );
+
+            //Order
+            $order = STM_LMS_Order::_user_orders($v->ID);
+            $order_ids = $order ? wp_list_pluck($order['posts'], 'id') : array();
+            $order_keys = $order ? wp_list_pluck($order['posts'], 'order_key') : array();
+            $order_dates = $order ? wp_list_pluck($order['posts'], 'date') : array();
+            $order_status = $order ? wp_list_pluck($order['posts'], 'status') : array();
+
+            foreach($order_dates as $sd => $singD) $order_dates[$sd] = date('m/d/Y', $singD);
+
+            return array(
+                'user_id' => $v->ID, 
+                'user_name' => $v->data->user_login, 
+                'email' => $v->data->user_email, 
+                'role' => $v->roles[0], 
+                'first_name' => get_user_meta( $v->ID, 'first_name', true ),
+                'last_name' => get_user_meta( $v->ID, 'last_name', true ), 
+                'degree' => $instructor_data ? $instructor_data['degree'] : '', 
+                'expertise' => $instructor_data ? $instructor_data['expertize'] : '',
+                'submission_date' => $submission_date, 
+                'answer_date' => $answer_date, 
+                'message' => isset($submission_history['message']) ? $submission_history['message'] : '', 
+                'password' => '', 
+                'send_reset_link' => '',
+                'course_id' => $cArray, 
+                'date_of_enrollment' => $sTime, 
+                'course_progress' => $progress, 
+                'completed_lesson_id' => $lessonsArray, 
+                'reset_progress' => '', 
+                'address' => $address, 
+                'organization' => $company,
+                'contact_number' => get_user_meta( $v->ID, 'billing_phone', true ), 
+                'order_by' => '', 
+                'order_no' => implode('-', $order_ids), 
+                'order_key' => implode('-', $order_keys), 
+                'order_status' => implode('-', $order_status), 
+                'order_date' => implode('-', $order_dates)
+            );
+        }, $users);
+
+        return new WP_REST_Response($users, 200);
+    }
 
     /**
      * Save csv data
@@ -383,6 +491,20 @@ class UIEMLMS_Api
                 }
             }
 
+            // WooCommerce User meta data
+            if(!empty($address)){
+                update_user_meta( $user_id, 'billing_address_1', $address );
+                update_user_meta( $user_id, 'shipping_address_1', $address );
+            }
+
+            if(!empty($organization)){
+                update_user_meta( $user_id, 'billing_company', $organization );
+                update_user_meta( $user_id, 'shipping_company', $organization );
+            }
+
+            if(!empty($contact_number)){
+                update_user_meta( $user_id, 'billing_phone', $contact_number );
+            }
         endforeach;
 
         return $data;
